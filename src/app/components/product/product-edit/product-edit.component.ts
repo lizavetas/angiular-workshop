76a7481a8126740ed71123aaef27d222a05afa5e
Elizaveta Shcherbakova <elizaveta.shcherbakova@idealo.de>
2017-08-22 11:59:39 +0200

    aufgabe 21

--- src/app/components/product/product-edit/product-edit.component.ts
+++ src/app/components/product/product-edit/product-edit.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../../../shared/product';
+
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+    public product: Product;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
