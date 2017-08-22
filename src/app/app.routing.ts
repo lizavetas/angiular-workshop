84a0e4a0b9c3c33ea448568fa70166a4ce2c6954
Elizaveta Shcherbakova <elizaveta.shcherbakova@idealo.de>
2017-08-22 10:03:50 +0200

    added router outlet

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductListComponent } from './components/product-list/product-list.component';
+
 // Define routes
-export const appRoutes: Routes = [];
+export const appRoutes: Routes = [
+    {
+        path: 'products',
+        component: ProductListComponent
+    }
+];
 
 // Create a new module with configured router
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
